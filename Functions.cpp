c94d03bddce6de6c95beb572ec79558809efc081
Kaio Cândido Santiago <kaio.santiago.13@hotmail.com>
2022-06-25 12:07:50 -0300

    Maior e menor número

---
+++ Functions.cpp
@@ -0,0 +1,7 @@
+void smallestAndBiggestNumber(int qty){
+    int array[qty];
+    for(int i = 0; i <= qty; i++){
+        array[i];
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int size, element, max = 0, min = 0;
+
+    cout << "Size of vector: " << endl;
+    cin >> size;
+    int array[size];
+    for(int i = 0; i < size; i++){
+        cout << "Say us one number: " << endl;
+        cin >> element;
+        array[i] = element;
+
+        bool arrayIsNull = array == NULL ? true : false;
+
+        if(arrayIsNull){
+            max = element;
+            min = element;
+        }
+
+        if(array[i] > max){
+            max = array[i];
+        }
+
+        if(array[i] < min){
+            min = array[i];
+        }
+    }
+
+    cout << "---------------" << endl;
+    cout << "Value of array: " << endl;
+    for(int x : array){
+        cout << x << endl;
+    }
+    cout << "Higher number: " << max << endl;
+    cout << "Smallest number: " << min << endl;
     return 0;
 }
